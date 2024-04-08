3936f8db6bb65b06fdd694da5b1cb263461ec2b1
Tatiana <138196468+Tanesha001@users.noreply.github.com>
2024-04-08 13:10:27 +0000

    feat: add solution 11

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -193,8 +193,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return () => {
+    const res = start;
+    start += 1;
+    return res;
+  };
 }
 
 module.exports = {
